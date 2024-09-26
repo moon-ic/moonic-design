251babbb1812cf4d4695a8556a7fb7edaadc20a2
moon-ic <3276817499@qq.com>
2024-09-26 21:30:35 +0800

    修改目录结构

---
+++ packages/components/vite.config.ts
@@ -0,0 +1,22 @@
+// packages/button/vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: 'index.ts',
+			name: 'Bydesign-components',
+			fileName: 'Bydesign-components'
+		},
+		minify: false,
+		rollupOptions: {
+			external: [/react.*/, 'react-dom'],
+			output: {
+				format: 'es',
+				dir: 'dist'
+			}
+		}
+	}
+});

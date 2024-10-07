66408d8e3d460f15487bfb3a495b5e720bea4ba5
moon-ic <3276817499@qq.com>
2024-10-07 16:20:52 +0800

    fix :menu.index add submenu

--- packages/components/Menu/index.tsx
+++ packages/components/Menu/index.tsx
@@ -1,14 +1,16 @@
 import { FC } from 'react';
 import Menu, { MenuProps } from './menu';
+import SubMenu, { SubMenuProps } from './subMenu';
 import MenuItem, { MenuItemProps } from './menuItem';
-import './styles/index.scss';
 import './styles/index.scss';
 
 export type MenuComponent = FC<MenuProps> & {
 	Item: FC<MenuItemProps>;
+	SubMenu: FC<SubMenuProps>;
 };
 
 const TransMenu = Menu as MenuComponent;
 TransMenu.Item = MenuItem;
+TransMenu.SubMenu = SubMenu;
 
 export default TransMenu;
